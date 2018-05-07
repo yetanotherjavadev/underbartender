9807beddbc6629b37abf8e6aed7589083509ddd0
PKarakata <pavlo.karakata@qvantel.com>
2018-05-07 18:47:44 +0300

    react impr

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,14 @@
-import React, {PureComponent} from 'react'
-import ArticleList from './ArticleList'
-import articles from '../fixtures'
-import 'bootstrap/dist/css/bootstrap.css'
+import React, {PureComponent} from 'react';
+import SelectorWidget from "./Selector/SelectorWidget";
 
 class App extends PureComponent {
-    state = {
-        reverted: false
-    };
     render() {
         console.log('---', 1)
         return (
-            <div className="container">
-                <div className="jumbotron">
-                    <h1 className="display-3">
-                        App name
-                        <button className="btn" onClick = {this.revert}>Revert</button>
-                    </h1>
-                </div>
-                <ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>
+            <div className="flexMain container">
+                <SelectorWidget/>
             </div>
         )
-    }
-
-    revert = () => {
-        this.setState({
-            reverted: !this.state.reverted
-        })
     }
 }
 
---
+++ src/components/Selector/SelectorWidget.js
@@ -0,0 +1,19 @@
+import React, {PureComponent} from 'react';
+
+class SelectorWidget extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    render() {
+        return (
+            <div className="omgdiv flexMain">aaa</div>
+        )
+    }
+}
+
+export default SelectorWidget;
